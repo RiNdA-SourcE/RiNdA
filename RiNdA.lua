6d958bd5096ccd25baa132484b7f364cd243b7bc
RiNdA-SourcE <88342091+RiNdA-SourcE@users.noreply.github.com>
2021-08-19 19:19:02 +0300

    Add files via upload

--- RiNdA.lua
+++ RiNdA.lua
@@ -10817,7 +10817,7 @@ else
 username = 'لا يوجد '
 end
 if result.status_.ID == "UserStatusRecently" and result.profile_photo_ ~= false then
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, mode.photos_[0].sizes_[1].photo_.persistent_id_,''..rdphoto..'\n ≁ ايديك < '..msg.sender_user_id_..'\n ≁ يـوزࢪ نـيم تبعڪ < '..username..'\n ≁ رتـبتك بـرو < '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ≁ موقـعك < '..rtpa..'\n ≁ تفاعلك < '..Total_Msg(Msguser)..'\n ≁ رسائلك < '..Msguser..'\n ≁ نسبه تفاعلك < '..string.sub(nspatfa, 1,5)..' %\n ≁ جفـصاتك < '..edit..'\n ≁ نقـاط تبـعك < '..NUMPGAME..'\n')
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, mode.photos_[0].sizes_[1].photo_.persistent_id_,''..rdphoto..'\n ≁ ايديك ↬ '..msg.sender_user_id_..'\n ≁ يـوزࢪ نـيم تبعڪ ↬ '..username..'\n ≁ رتـبتك بـرو ↬ '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ≁ موقـعك ↬ '..rtpa..'\n ≁ تفاعلك ↬ '..Total_Msg(Msguser)..'\n ≁ رسائلك ↬ '..Msguser..'\n ≁ نسبه تفاعلك ↬ '..string.sub(nspatfa, 1,5)..' %\n ≁ جفـصاتك ↬ '..edit..'\n ≁ نقـاط تبـعك ↬ '..NUMPGAME..'\n')
 else 
 if result.status_.ID == "UserStatusEmpty" and result.profile_photo_ == false then
 send(msg.chat_id_, msg.id_,'[\n ≁ ايديك < '..msg.sender_user_id_..'\n ≁ معرفك < '..username..'\n ≁ رتـبتك بـرو < '..Rutba(msg.sender_user_id_,msg.chat_id_)..'\n ≁ موقـعك < '..rtpa..'\n ≁ تفاعلك < '..Total_Msg(Msguser)..'\n ≁ رسائلك < '..Msguser..'\n ≁ نسبه  تفاعلك < '..string.sub(nspatfa, 1,5)..' %\n ≁ جفـصاتك < '..edit..'\n ≁ نقـاط تبـعك < '..NUMPGAME..']\n')
